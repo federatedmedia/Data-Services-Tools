84af7dd8dfa307dd804c0df0703b413ebc3ddc22
Nate Vogel <natevogel@gmail.com>
2013-10-23 10:19:39 -0700

    add license verbiage

--- workflow_runner.py
+++ workflow_runner.py
@@ -3,7 +3,23 @@
 @version 1.1
 @lastupdate 2013-10
 @author Nate Vogel, nvogel@fmpub.net
+
 @license GPLv2, federated media publishing, www.federatedmedia.net
+
+    This file is part of Data-Services-Tools.
+
+    Data-Services-Tools is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Data-Services-Tools is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Data-Services-Tools.  If not, see <http://www.gnu.org/licenses/>.
 
 Purpose:
     This application executes a sequence of commands that make up a workflow.
